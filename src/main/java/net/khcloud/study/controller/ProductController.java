f83f341a4ce5aa19cd79e539ae21f1a552f56d6e
shengyf <shengyf@adtec.com.cn>
2023-02-16 16:09:05 +0800

    把逻辑代码拆到service层

--- src/main/java/net/khcloud/study/controller/ProductController.java
+++ src/main/java/net/khcloud/study/controller/ProductController.java
@@ -4,6 +4,7 @@ import net.khcloud.study.exception.ProductNotFoundException;
 import net.khcloud.study.dao.domain.Product;
 import net.khcloud.study.dao.mapper.ProductMapper;
 import io.swagger.annotations.Api;
+import net.khcloud.study.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,29 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by GBC on 2017/10/20.
+ * Created by SYF on 2023/01/16.
  */
 @Api
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
     @Autowired
-    private ProductMapper productMapper;
+    private ProductService productService;
 
     @GetMapping("/{id}")
     public Product getProductInfo(@PathVariable("id") Long productId) {
-        return productMapper.select(productId);
+        return productService.getProduct(productId);
     }
 
-    @PutMapping("/{id}")
-    public Product updateProductInfo(@PathVariable("id") Long productId, @RequestBody Product newProduct) {
-        Product product = productMapper.select(productId);
-        if (product == null) {
-            throw new ProductNotFoundException(productId);
-        }
-        product.setName(newProduct.getName());
-        product.setPrice(newProduct.getPrice());
-        productMapper.update(product);
-        return product;
+    @PutMapping("")
+    public Product updateProductInfo(@RequestBody Product product) {
+        return productService.updateProduct(product);
     }
 }
